37f627a83126e01fc4d77de99730e99fe85e82ae
Panu Som <spike_hammer@hotmail.com>
2019-11-06 17:53:44 +0700

    homework 3 success

--- react-film/src/App.js
+++ react-film/src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
+import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
-import FilmListing from './components/FilmListing';
-import FilmDetails from './components/FilmDetails';
-import './App.css';
-
-const { films } = TMDB;
 
 class App extends Component {
+  
+  state = {
+    films: TMDB.films,
+    faves: [],
+    current: {} 
+  }
+
+  handleFaveToggle = film => {
+
+    let faves = [...this.state.faves];
+    let filmIndex = faves.indexOf(film); 
+
+    if(filmIndex < 0) {
+      faves.push(film);
+    } else {
+      faves.splice(filmIndex, 1);
+    }
+
+    this.setState({faves});
+
+  }
+
+  handleFilmRowToggle = film => {
+    console.log(film);
+  }
+
   render() {
     return (
       <div className="film-library">
-        <FilmListing films={films} />
-        <FilmDetails films={films} />
+        <FilmListing films={this.state.films} faves={this.state.faves} onFaveToggle={this.handleFaveToggle} onFilmRowToggle={this.handleFilmRowToggle} />
+        <FilmDetails films={this.state.films} current={this.state.current}  />
       </div>
     );
   }
